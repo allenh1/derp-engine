c9c342c84f9d6aed075789a52a86ca1a9b84ddd3
Ana-Gabriel <ana.gabriel2012@gmail.com>
2016-11-13 13:50:54 -0500

    multi slash fix

--- src/ParseHTML.cpp
+++ src/ParseHTML.cpp
@@ -51,7 +51,7 @@ bool ParseHTML::operator() () {
 		case QUOTE:
 			if(url.size()>0) {
 				if(!url.contains("http://")&&!url.contains("https://")) {
-					if(url[0] == '/') url = m_url+url;
+					if(url[0] == '/' || url[url.size()-1]=='/') url = m_url+url;
 					else url = m_url+'/'+url;
 				} if(!url.contains("mailto:") && !url.contains(".tar")) {
 					std::cerr<<"url: "<<url.toStdString()<<std::endl;
